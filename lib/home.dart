50fc45c3ce328f6f751d4cfdfb09651318ef14d3
Khalid Ibrahim <fxtryio@gmail.com>
2021-11-05 10:22:47 +0800

    cleanup comments and loading screen

--- lib/home.dart
+++ lib/home.dart
@@ -15,58 +15,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List userData = [];
-  // List userData = [
-  //   {
-  //     "id": 1,
-  //     "first_name": "Billie",
-  //     "last_name": "Redgewell",
-  //     "username": "bredgewell0",
-  //     "last_seen_time": "9:30 AM",
-  //     "avatar":
-  //         "https://robohash.org/doloribusiuredolor.png?size=50x50&set=set1",
-  //     "status": "policy"
-  //   },
-  //   {
-  //     "id": 2,
-  //     "first_name": "Jacobo",
-  //     "last_name": "Sporton",
-  //     "username": "jsporton1",
-  //     "last_seen_time": "3:17 AM",
-  //     "avatar":
-  //         "https://robohash.org/suscipitplaceatut.png?size=50x50&set=set1",
-  //     "status": "Implemented"
-  //   },
-  //   {
-  //     "id": 3,
-  //     "first_name": "Shae",
-  //     "last_name": "Muldoon",
-  //     "username": "smuldoon2",
-  //     "last_seen_time": "2:17 PM",
-  //     "avatar":
-  //         "https://robohash.org/laudantiumsitvoluptatem.png?size=50x50&set=set1",
-  //     "status": "Re-engineered",
-  //     "messages": 2
-  //   },
-  //   {
-  //     "id": 4,
-  //     "first_name": "Weidar",
-  //     "last_name": "Lowen",
-  //     "username": "wlowen3",
-  //     "last_seen_time": "2:13 AM",
-  //     "avatar": "https://robohash.org/utquiin.png?size=50x50&set=set1",
-  //     "status": "Inverse"
-  //   },
-  //   {
-  //     "id": 5,
-  //     "first_name": "Ado",
-  //     "last_name": "Hallbord",
-  //     "username": "ahallbord4",
-  //     "last_seen_time": "5:17 AM",
-  //     "avatar":
-  //         "https://robohash.org/sitexplicabovoluptas.png?size=50x50&set=set1",
-  //     "status": "adapter"
-  //   },
-  // ];
 
   Future<void> readJson() async {
     final String response = await rootBundle.loadString('mockdata.json');
@@ -91,26 +39,25 @@ class _HomePageState extends State<HomePage> {
         body: Container(
             width: double.infinity,
             child: Center(
-              child: ListView(
-                  padding: const EdgeInsets.all(8),
-                  children: userData.map((user) {
-                    return Column(children: [
-                      ItemCard(
-                          user: User(
-                              id: user['id'],
-                              first_name: user['first_name'],
-                              last_name: user['last_name'],
-                              last_seen_time: user['last_seen_time'],
-                              username: user['username'],
-                              avatar: user['avatar'],
-                              status: user['status'],
-                              messages: user['messages'])),
-                      const Divider(),
-                    ]);
-                  }).toList()),
+              child: userData.isNotEmpty
+                  ? ListView(
+                      padding: const EdgeInsets.all(8),
+                      children: userData.map((user) {
+                        return Column(children: [
+                          ItemCard(
+                              user: User(
+                                  id: user['id'],
+                                  first_name: user['first_name'],
+                                  last_name: user['last_name'],
+                                  last_seen_time: user['last_seen_time'],
+                                  username: user['username'],
+                                  avatar: user['avatar'],
+                                  status: user['status'],
+                                  messages: user['messages'])),
+                          const Divider(),
+                        ]);
+                      }).toList())
+                  : const Text("Loading..."),
             )));
   }
 }
-
-// child: Text("Welcome home!",
-//                 style: TextStyle(fontSize: 30, color: Colors.teal))));
